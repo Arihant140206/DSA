ea8700c6a790b53653613744f0c732aebc41dfd2
Arihant140206 <arihantjain140602@gmail.com>
2025-11-29 12:14:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.30%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 1) return 1;
+
+        int prev1 = 1;  // ways to reach step 0
+        int prev2 = 1;  // ways to reach step 1
+        int curr;
+
+        for (int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+
+        return prev2;
+    }
+};
