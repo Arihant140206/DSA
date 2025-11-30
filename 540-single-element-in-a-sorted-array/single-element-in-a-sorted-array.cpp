a2f3777819b2e344c6adfd356e71106094c3323f
Arihant140206 <arihantjain140602@gmail.com>
2025-11-30 22:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.33%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int res=0;
-        for(int num:nums){
-        res^=num;
-        }
-        return res;
-    }
+        int s = 0, e = nums.size() - 1;
 
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+
+            // Make mid always point to even index
+            if (mid % 2 == 1)
+                mid--;
+
+            if (nums[mid] == nums[mid + 1])
+                s = mid + 2;   // pair is complete, go right
+            else
+                e = mid;       // broken pair, go left
+        }
+
+        return nums[s];
+    }
 };
