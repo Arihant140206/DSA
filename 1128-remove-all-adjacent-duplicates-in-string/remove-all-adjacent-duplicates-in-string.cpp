0ee239f444a2b0351c73a6c7d84e896f3e7c4ce1
Arihant Jain <arihantjain140602@gmail.com>
2026-06-06 00:16:04 +0530

    Time: 3 ms (80.57%) | Memory: 14.3 MB (31.61%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-//stack ko use karke question kana hai (pratyush video 37 ),simply ek ek karke stack me push karo and keep checking the top elememt with the upcoming element of the string , if they are same pop the on e from the stack and keep moving ahead in string , keep doing it until the end ,. fin ally store the answer in res and reverse it in the end as stack will give ulta ans on popping and we want in form of string 
     string removeDuplicates(string s) {
-        int n=s.size();
-        stack<char>st;
-        int i;
-        string res;
+        stack<char> st;
 
-        for(i=0;i<n;i++){
-            if(st.empty()){
-                st.push(s[i]);
-                continue;
+        for(char ch : s) {
+            if(st.empty()) {
+                st.push(ch);
             }
-            if(st.top()==s[i]){
+            else if(st.top() == ch) {
                 st.pop();
-                continue;
             }
-            st.push(s[i]);
-
-            //finmal ans me dalna and reverse karna 
+            else {
+                st.push(ch);
+            }
         }
-            while(!st.empty()){
-                res.push_back(st.top());
-                st.pop();
-            }
-            reverse(res.begin(),res.end());
-            //
 
+        string res;
+        while(!st.empty()) {
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
         return res;
     }
 };
