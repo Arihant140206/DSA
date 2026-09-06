32c25fce43975ee11e23262ab0a2c52d84d0f8a0
Arihant Jain <arihantjain140602@gmail.com>
2026-09-06 08:43:45 +0530

    Time: 48 ms (51.95%) | Memory: 29.2 MB (28.45%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) {
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>finalans;
+        int left,right;
+        int val=0;
+        for(val=0;val<nums.size();val++){
+        if (val > 0 && nums[val] == nums[val - 1])
+        continue;
+        left=val+1;
+        right=nums.size()-1;
+        {
+            while(left<right)
+            {
+                int target=-1*(nums[val]);
+                int sum=nums[left]+nums[right];
+                if(sum==target)
+                {
+                    finalans.push_back({nums[left], nums[right], nums[val]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                     left++;
 
-        int n = a.size();
-        vector<vector<int>> ans;
-
-        sort(a.begin(), a.end());
-
-        for(int i = 0; i < n - 2; i++) {
-
-            if(i > 0 && a[i] == a[i-1])
-                continue;
-
-            int l = i + 1;
-            int r = n - 1;
-            int sum = -a[i];
-
-            while(l < r) {
-
-                int s = a[l] + a[r];
-
-                if(s == sum) {
-
-                    ans.push_back({a[i], a[l], a[r]});
-
-                    l++;
-                    r--;
-
-                    while(l < r && a[l] == a[l-1])
-                        l++;
-
-                    while(l < r && a[r] == a[r+1])
-                        r--;
-                }
-
-                else if(s < sum)
-                    l++;
-
+                    while (left < right && nums[right] == nums[right + 1])
+                     right--;
+                    
+                 }
+                
+                else if(sum<target)  
+                {
+                    left++;
+                }         
                 else
-                    r--;
+                right--;
             }
         }
-
-        return ans;
+        }return finalans;
     }
 };
