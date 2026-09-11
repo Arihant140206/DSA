24c0b489958eef0eb0f1565bd510344866ca9b19
Arihant Jain <arihantjain140602@gmail.com>
2026-09-11 20:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,7 +1,7 @@
-class Solution {
+/*class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxsum=nums[0];
+        int maxsum=nums[0];//imp
         int currentsum=0;
         int n=nums.size();
 
@@ -17,4 +17,35 @@ public:
 
     }
     
+};*/
+
+class Solution {
+public:
+    
+    // recursive function
+    int solve(vector<int>& nums, int i, int currentSum, int maxSum) {
+        
+        // base case
+        if(i==nums.size())
+        return maxSum;
+        
+        // process current element (update currentSum)
+        currentSum=max(nums[i],currentSum+nums[i]);
+        
+        // update maxSum
+        maxSum=max(maxSum,currentSum);
+        
+        // recursive call (move to next index)
+        return solve(nums,i+1,currentSum,maxSum);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        
+        // initial call with:
+        // index = 0
+        // currentSum = ?
+        // maxSum = ?
+        return solve(nums,0,0,nums[0]);
+        
+    }
 };
