3093bf23295030c2a06ccdbd06c5c7ac80dea608
Arihant Jain <arihantjain140602@gmail.com>
2026-09-12 22:01:13 +0530

    Time: 4 ms (22.99%) | Memory: 12.9 MB (15.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int>st;
         unordered_map<int,int>mp;
-        
-        //nums2 ke saare lemenets ko hashmap =me store karo
-        for(int i=0;i<nums2.size();i++){
-            while(!st.empty()&&nums2[i]>st.top()){
-                mp[st.top()]=nums2[i];
+        vector<int>ans;
+        stack<int>st;
+        for(int i=0;i<nums2.size();i++)
+        {
+            while(!st.empty()&&nums2[i]>nums2[st.top()])
+            {
+                mp[nums2[st.top()]]=nums2[i];
                 st.pop();
             }
-            st.push(nums2[i]);
+            st.push(i);
+            mp[nums2[st.top()]]=-1;
         }
-        while(!st.empty()){
-            mp[st.top()]=-1;
-            st.pop();
-        }
-        //hashmap ban gaya ab answer 
-        vector<int>ans;
-        for(int i=0;i<nums1.size();i++){
+        //jo back gaye , jinka nahi mil paaya greater element unk0 -1 dedo
+        for(int i=0;i<nums1.size();i++)
+        {
             ans.push_back(mp[nums1[i]]);
-        }
-        return ans;
+        }return ans;
     }
 };
