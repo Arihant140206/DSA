e97012d94663b818de9456b834ee6c9584e3146e
Arihant Jain <arihantjain140602@gmail.com>
2026-09-14 22:55:24 +0530

    Time: 3 ms (48.80%) | Memory: 11.8 MB (46.92%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(char ch:s)
+        {
+            if(st.empty())
+            st.push(ch);
+            else if(ch=='B'&& st.top()=='A')
+            {
+                st.pop();
+                continue;
+            }
+            else if(ch=='D'&& st.top()=='C')
+            {
+                st.pop();
+                continue;
+            }
+            else{
+                st.push(ch);
+            }
+        }return st.size();
+    }
+};
