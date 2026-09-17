c5b8829a79ecf7633a066f6fad1eb749a16414df
Arihant Jain <arihantjain140602@gmail.com>
2026-09-17 21:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (15.95%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,15 +3,15 @@ public:
     int findPeakElement(vector<int>& nums) {
         int start = 0, end = nums.size() - 1;
 
-        while (start < end) {
+        while(start < end) {
             int mid = start + (end - start) / 2;
-
-            if (nums[mid] < nums[mid + 1])
-                start = mid + 1;//go towards increasing slope where peak is present 
-            else
-                end = mid;//go towards decresasing slo[e -left side  
+//agar abhi ka element agle element se chota hai toh aage badho
+            if(nums[mid] < nums[mid + 1])
+                start = mid + 1;
+            else//abhi wala bada hai toh end ko udhar le aao , ho sakta hai peak ho hehe
+                end = mid;
         }
-
-        return start;
+//peak tab milega jabb start and end mil jaayenge 
+        return end;
     }
 };
