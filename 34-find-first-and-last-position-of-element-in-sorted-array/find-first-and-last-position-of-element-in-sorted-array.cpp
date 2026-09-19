3ae42054f1110a0d390e168ecb0ee1214ce83507
Arihant Jain <arihantjain140602@gmail.com>
2026-09-19 22:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.46%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int first = -1, last = -1, mid;
+        int l = 0;
+        int r = nums.size() - 1;
 
-        // Find first occurrence
-        while (start <= end) {
-            mid = (start + end) / 2;
+        int ans1 = -1;
+        int ans2 = -1;
+
+        // Find FIRST occurrence
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
             if (nums[mid] == target) {
-                first = mid;       // store position
-                end = mid - 1;     // keep searching left
+                ans1 = mid;
+                r = mid - 1;
             }
             else if (nums[mid] < target) {
-                start = mid + 1;
+                l = mid + 1;
             }
             else {
-                end = mid - 1;
+                r = mid - 1;
             }
         }
 
-        start = 0, end = nums.size() - 1;
+        // Reset
+        l = 0;
+        r = nums.size() - 1;
 
-        // Find last occurrence
-        while (start <= end) {
-            mid = (start + end) / 2;
+        // Find LAST occurrence
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
             if (nums[mid] == target) {
-                last = mid;        // store position
-                start = mid + 1;   // keep searching right
+                ans2 = mid;
+                l = mid + 1;
             }
             else if (nums[mid] < target) {
-                start = mid + 1;
+                l = mid + 1;
             }
             else {
-                end = mid - 1;
+                r = mid - 1;
             }
         }
 
-        return {first, last};
+        return {ans1, ans2};
     }
 };
